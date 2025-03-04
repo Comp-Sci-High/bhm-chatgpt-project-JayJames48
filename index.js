44cf821ac802097f3d8857d9e27ce908831a0fc4
JayJames48 <jayden.james25@compscihigh.org>
2025-03-04 16:05:25 +0000

    finished

--- index.js
+++ index.js
@@ -12,4 +12,6 @@ app.use((req, res, next) => {
   app.get("/",(req,res)=>{
     res.sendFile(__dirname + "/public/index.html")
   })
-  
+  app.listen(3000, (req, res) => {
+    console.log("hey")
+    })
